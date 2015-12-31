24be898260d5f45db30b31be592dcc7ff727e1ec
Comforse <comforse.github@gmail.com>
2015-12-31 16:03:44 +0100

    Missing HtmlServiceProvider dependency

--- config/app.php
+++ config/app.php
@@ -138,6 +138,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Illuminate\Html\HtmlServiceProvider::class,
         Rooles\RoolesServiceProvider::class,
+        Illuminate\Html\HtmlServiceProvider::class,
         /*
          * Application Service Providers...
          */
